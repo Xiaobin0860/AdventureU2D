b5f667bede56d102127e444660317193b41c33ef
Li Xiaobin <l_xb@foxmail.com>
2021-02-06 23:10:40 +0800

    Physics System: Blocking Movement; Collectibles

---
+++ Assets/Scripts/HealthCollectiable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectiable : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other + " entered the trigger");
+        RubyController controller = other.GetComponent<RubyController>();
+        if (controller)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return curHealth; } }
+    int curHealth;
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        curHealth = maxHealth;
+        Debug.Log("curHealth=" + curHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x += 3.0f * horizontal * Time.deltaTime;
-        position.y += 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        curHealth = Mathf.Clamp(curHealth + amount, 0, maxHealth);
+        Debug.Log(curHealth + "/" + maxHealth);
     }
 }
