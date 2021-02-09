769e03fb14aaf4547d0d87e03d676c2fd0197551
Li Xiaobin <l_xb@foxmail.com>
2021-02-09 23:43:47 +0800

    Particles

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,8 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2d;
     float timer;
     int direction = 1;
@@ -76,6 +78,8 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+
+        smokeEffect.Stop();
     }
 
 }
